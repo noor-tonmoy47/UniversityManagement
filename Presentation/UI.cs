466c171fe1164523fc4c361ddbe412070b4b2378
noor-tonmoy47 <noortonmoy47@gmail.com>
2024-10-08 17:47:13 +0600

    prompt for users

--- Presentation/UI.cs
+++ Presentation/UI.cs
@@ -10,5 +10,50 @@ namespace Presentation
     {
 
 
+        private static int Prompt()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Choose an option");
+            Console.WriteLine("1. Register");
+            Console.WriteLine("2. View Data");
+            Console.WriteLine("\n");
+
+
+            string input;
+            int choice = 0;
+
+            while (true)
+            {
+                try
+                {
+                    input = Console.ReadLine();
+
+                    if (!input.Equals("1") && !input.Equals("2"))
+                    {
+                        throw new Exception();
+                    }
+
+                    choice = Int32.Parse(input);
+
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Please input 1 or 2");
+                    continue;
+                }
+
+                break;
+            }
+
+
+            return choice;
+        }
+        static void Main(string[] args)
+        {
+            int choice = Prompt();
+
+        }
+
     }
 }
