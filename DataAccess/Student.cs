04ac8395b04015f05ffe9ad00da965f3cef3324f
noor-tonmoy47 <noortonmoy47@gmail.com>
2024-10-08 17:31:49 +0600

    Created Student Class

--- DataAccess/Student.cs
+++ DataAccess/Student.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataAccess
+namespace UniManagement
 {
     internal class Student
     {
+        public virtual int ID { get; set; }
+        public virtual string FirstName { get; set; }
+
+        public virtual string LastName { get; set; }
+
+
+        public virtual string Department { get; set; }
+
+        public virtual int Age { get; set; }
+
+
     }
 }
