50ea35d7709515653f2dec64512fc1a8dd3a40b3
noor-tonmoy47 <noortonmoy47@gmail.com>
2024-10-10 17:00:33 +0600

    Change in Code Structure and solution

--- DataAccess/HibernateCon.cs
+++ DataAccess/HibernateCon.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Reflection;
+using System.Data.SqlClient;
 using NHibernate;
 using NHibernate.Cfg;
 using NHibernate.Dialect;
 using NHibernate.Driver;
-
 namespace DataAccess
 {
-    internal class HibernateCon
+    public class HibernateCon
     {
 
         private static readonly string conString = "Data Source=OATHKEEPER;Initial Catalog=University;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
 
-        
+        private ISessionFactory sessionFactory;
+
+        public HibernateCon()
+        {
+            this.sessionFactory = ConnectDB();
+            
+        }
 
         private static ISessionFactory ConnectDB()
         {
@@ -25,91 +31,28 @@ namespace DataAccess
                 x.Dialect<MsSql2008Dialect>();
             });
 
-            conf.AddAssembly(Assembly.GetExecutingAssembly());
+            //conf.AddAssembly(Assembly.GetExecutingAssembly());
 
             return conf.BuildSessionFactory();
-
         }
 
-        private static void CreateSession(ISessionFactory sessionFactory) {
-            using (var session = sessionFactory.OpenSession())
+        public void CreateData(Student student)
+        {
+            using (var session = this.sessionFactory.OpenSession())
             {
-
                 using (var tx = session.BeginTransaction())
                 {
+                    Console.WriteLine("Database Connected");
 
-                    //CRUD operations
+                    //create student
+                    session.Save(student);
 
-                    if(Prompt() == 1)
-                    {
-                        // Registration Logic
-                    }
-                    else
-                    {
-                        // Data Presenting Logic
-                    }
+
                     tx.Commit();
                 }
+
             }
         }
-
-
-        private static int Prompt()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Choose an option");
-            Console.WriteLine("1. Register");
-            Console.WriteLine("2. View Data");
-            Console.WriteLine("\n");
-
-
-            string input;
-            int choice = 0;
-
-            while (true)
-            {
-                try
-                {
-                    input = Console.ReadLine();
-
-                    if (!input.Equals("1") && !input.Equals("2"))
-                    {
-                        throw new Exception();
-                    }
-
-                    choice = Int32.Parse(input);
-
-                }
-                catch (Exception)
-                {
-
-                    Console.WriteLine("Please input 1 or 2");
-                    continue;
-                }
-
-                break;
-            }
-           
-
-            return choice;
-        }
-
-        private void Create()
-        {
-
-        }
-        static void Main(string[] args)
-        {
-            
-
-            var sessionFactory = ConnectDB();
-
-
-            //open session
-
-            CreateSession(sessionFactory);
-
-        }
-
+      
     }
 }
