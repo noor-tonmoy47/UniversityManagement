4d7419c90548a6801b6031975a50d49b57eae0d4
noor-tonmoy47 <noortonmoy47@gmail.com>
2024-10-10 16:57:41 +0600

    Created Controller class to handle business logic

---
+++ Logic/Controller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using DataAccess;
+using NHibernate;
+namespace Logic
+{
+    public class Controller
+    {
+        // create\
+
+        private HibernateCon hCon;
+
+        public Controller()
+        {
+            hCon = new HibernateCon();
+        }
+        public void AddData(string FirstName, string LastName, string StringAge, string Department)
+        {
+            int ageInt;
+
+            while (true)
+            {
+
+                try
+                {
+                    ageInt = Int32.Parse(StringAge);
+                } catch (Exception e){
+                    Console.WriteLine("invalid input");
+                    continue;
+                }
+
+                break;
+            }
+
+            Student student = new Student
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Age = ageInt,
+                Department = Department
+            };
+            
+            this.hCon.CreateData(student);
+
+        }
+        //delete
+
+        //show all
+
+
+    }
+}
